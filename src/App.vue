4d7bf1c51c63f98703ea40bad671e46b6efc12da
kapolandia <paykapo4@gmail.com>
2024-04-17 11:12:43 +0200

    push ex

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script setup>
   import AppHeader from './components/AppHeader.vue'
+  import HomeJumbotron from './components/HomeJumbotron.vue'
+  import ComicsGrid from './components/ComicsGrid.vue';
+  import AppIcons from './components/AppIcons.vue';
+  import AppNav from "./components/AppNav.vue"
+	import AppContacts from "./components/AppContacts.vue"
 </script>
 
 <template>
   <AppHeader></AppHeader>
+  <HomeJumbotron></HomeJumbotron>
+  <ComicsGrid></ComicsGrid>
+  <AppIcons></AppIcons>
+	<footer>
+		<AppNav></AppNav>
+		<AppContacts></AppContacts>
+	</footer>
 </template>
 
-<style scoped>
-
+<style lang="scss">
+	@use "./style/generic";
 </style>
